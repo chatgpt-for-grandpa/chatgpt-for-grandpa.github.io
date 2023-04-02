6193fd6a2bfa6660561ac671cc29ab34fc14f3e3
Tdxdxoz <tdxdxoz@gmail.com>
2023-04-02 09:29:49 +0800

    update test text

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders send button", () => {
   render(<App />);
   const sendButton = screen.getByText("发送");
   expect(sendButton).toBeInTheDocument();
