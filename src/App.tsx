74563648faf4aae52d52bf291f9a6b9acaa34439
Tdxdxoz <tdxdxoz@gmail.com>
2023-04-08 16:54:36 +0800

    change nav ui to keep same

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import "./App.css";
+import "./App.scss";
 import ProjectIntro from "./project_info";
 import Tabs from "./tabs";
 
@@ -23,13 +23,18 @@ function App() {
   const [fontSize, setFontSize] = useState(1);
   const fontSizeClasses = ["font-sm", "", "font-lg", "font-xl"];
   const [showAlert, setShowAlert] = useState(true);
+  const [isSwitching, setIsSwitching] = useState(false);
 
   const handleFontSizeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFontSize(parseInt(e.target.value, 10));
   };
 
   return (
-    <>
+    <div
+      className={`App ${fontSizeClasses[fontSize]}${
+        isSwitching ? " other-ui" : ""
+      }`}
+    >
       {showAlert && (
         <>
           {NOT_MAIN_ADDRESS && (
@@ -59,10 +64,15 @@ function App() {
           )}
         </>
       )}
-      <Nav variant="tabs" defaultActiveKey="#">
+      <Nav
+        variant="tabs"
+        defaultActiveKey="#"
+        className="my-2"
+        onSelect={(ek) => setIsSwitching(ek !== "#")}
+      >
         <Nav.Item>
           <Nav.Link href="https://www.13042332817.top/chat-student">
-            青春界面
+            暗黑界面
           </Nav.Link>
         </Nav.Item>
         <Nav.Item>
@@ -71,10 +81,10 @@ function App() {
           </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="#">聊天界面</Nav.Link>
+          <Nav.Link href="#">青春界面</Nav.Link>
         </Nav.Item>
       </Nav>
-      <Container fluid="md" className={`App pt-4 ${fontSizeClasses[fontSize]}`}>
+      <Container fluid="md">
         <h1>聊天机器人</h1>
         <ProjectIntro />
         <Row>
@@ -93,7 +103,7 @@ function App() {
         </Row>
         <Tabs />
       </Container>
-    </>
+    </div>
   );
 }
 
